643fdbb81125b84326e6b0ef5dfe0498fdb3893c
Mariami Tetradze <mariamtetradze9@gmail.com>
2021-06-29 17:38:45 +0400

    add fileupload and jsexecutor tests

---
+++ src/test/java/JSexecutor.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class JSexecutor {
+    WebDriver driver;
+    @BeforeMethod
+    public void beforeTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void hoverItem(){
+        driver.get("http://webdriveruniversity.com/To-Do-List/index.html ");
+        driver.manage().window().maximize();
+        WebElement practiseMagicItem = driver.findElement(By.xpath("//div[@id=\"container\"]//ul//li[contains(text(), \" Practice magic\")]"));
+        Actions action = new Actions(driver);
+        action.moveToElement(practiseMagicItem).perform();
+        WebElement deleteButton = driver.findElement(By.xpath("//div[@id=\"container\"]//ul//li[contains(text(), \" Practice magic\")]//span//i"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", deleteButton);
+
+    }
+
+    @Test
+    public void scrollAndValidate(){
+        driver.get("http://webdriveruniversity.com/Scrolling/index.html ");
+        driver.manage().window().maximize();
+        JavascriptExecutor js= (JavascriptExecutor)driver;
+        WebElement box = driver.findElement(By.id("zone2-entries"));
+        js.executeScript("arguments[0].scrollIntoView();",box);
+        String text = js.executeScript("return document.getElementById('zone2-entries').innerHTML").toString();
+        System.out.println("Visible text is "+text );
+}
+    @AfterMethod
+    public void closeDriver() {
+        driver.close();
+    }
+}
